65c9d349db363a89f97431e22b363a8b10f3877e
Rafael <rafael@localhost.localdomain>
2018-09-25 22:45:29 -0300

    Com a função get_size_struct

--- grava_structs.c
+++ grava_structs.c
@@ -2,6 +2,34 @@
 /* Rafael Damazio    1712990  3WB */
 
 #include <stdio.h>
+#include <string.h>
+
+int get_size_struct(char* campos){
+	int i;
+	int bigger_size=0;
+	int size_field=0;
+	int sum_fields=0;
+	for(i=0;i<strlen(campos);i++){
+		switch (campos[i]){
+	
+			case 'c':
+				size_field=sizeof(char);
+				break;
+			case 's':
+				size_field=sizeof(short int);
+				break;
+			case 'i':
+				size_field=sizeof(int);
+				break;
+			case 'l':
+				size_field=sizeof(long int);
+				break;
+		}
+		sum_fields+=size_field;
+		if(size_field>bigger_size) bigger_size=size_field;
+	}
+	return (sum_fields/bigger_size+sum_fields%bigger_size)*bigger_size;
+}/* fim da função get_size_struct */
 
 
 int grava_structs (int nstructs, void *valores, char *campos, char ord, char *arquivo){
@@ -10,7 +38,7 @@ int grava_structs (int nstructs, void *valores, char *campos, char ord, char *ar
 	unsigned char byte = 0;
 	int ncampos = 0, i = 0,j;
 	int pAux = 0;
-	
+	int size_struct = get_size_struct(campos);
 	fwrite(&nstructs,sizeof(char),1,fname);
 
 	while( campos[ncampos] != '\0' ){
@@ -59,10 +87,8 @@ int grava_structs (int nstructs, void *valores, char *campos, char ord, char *ar
 			
 			j = 0; /* contagem dos campos de cada struct */
 			pAux = 0; /* pAux é um inteiro que varia de 0 até o tamanho da struct */
-			while(pAux < sizeof(*valores)){
-			
+			while(pAux < size_struct){
 				int size_valor; /* size_valor é o tamanho em bytes do campo atual */
-				
 				switch(campos[j]){
 					case 'c':
 						size_valor = sizeof(char);
@@ -77,9 +103,11 @@ int grava_structs (int nstructs, void *valores, char *campos, char ord, char *ar
 						size_valor = sizeof(long int);
 						break;
 				} /* switch */
-				
+				//printf("%d\n",pAux);				
+				//printf("%d\n",j);
 				pAux = ((pAux-1)/size_valor + 1)*size_valor; /* Ajuste do endereço ao campo */
-				fwrite((valores+i*sizeof(*valores)+pAux),size_valor,1,fname); /* Escrita */
+				
+				fwrite((valores+i*size_struct+pAux),size_valor,1,fname); /* Escrita */
 				pAux += size_valor; /* Incremento do ponteiro auxiliar pelo tamanho do campo */
 				j += 1;
 				
--- teste.c
+++ teste.c
@@ -3,11 +3,20 @@
 */
 
 #include <stdio.h>
-#include "grava_structs.h"
+
+typedef struct a{
+	int i;
+	long l;
+	short s;
+	char c;
+	short s2;
+} structs;
+
 
 int main(void)
 {
-   
-
+   structs a[3]={{1,90,4,14,34},{2,91,5,15,35},{3,93,6,16,36}};
+   grava_structs(3,a,"ilscs",'L',"aaa.txt");
+ 
    return 0;
 }
