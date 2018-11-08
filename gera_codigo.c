551702fe554e4a6ae9045021b6fcf8e04bf2e850
GUILHERME DANTAS DE OLIVEIRA <e1713155@tafe.grad.inf.puc-rio.br>
2018-11-08 17:59:10 -0200

    Avanços

---
+++ gera_codigo.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "gera_codigo.h"
+
+static void error (const char *msg, int line) {
+  fprintf(stderr, "erro %s na linha %d\n", msg, line);
+  exit(EXIT_FAILURE);
+}
+
+void gera_codigo (FILE *f, void **code, funcp *entry) {
+
+  int line = 1;
+  int  c;
+
+  while ((c = fgetc(f)) != EOF) {
+    switch (c) {
+      case 'f': { /* function */
+        char c0;
+        if (fscanf(f, "unction%c", &c0) != 1)
+          error("comando invalido", line);
+        printf("function\n");
+        //
+        break;
+      }
+      case 'e': { /* end */
+        char c0;
+        if (fscanf(f, "nd%c", &c0) != 1)
+          error("comando invalido", line);
+        printf("end\n");
+        break;
+      }
+      case 'r': {  /* retorno incondicional */
+        int idx0, idx1;
+        char var0, var1;
+        if (fscanf(f, "et %c%d", &var0, &idx0) != 2) 
+          error("comando invalido", line);
+        printf("ret %c%d\n", var0, idx0);
+        break;
+      }
+      case 'z': {  /* retorno condicional */
+        int idx0, idx1;
+        char var0, var1;
+        if (fscanf(f, "ret %c%d %c%d", &var0, &idx0, &var1, &idx1) != 4) 
+          error("comando invalido", line);
+        printf("zret %c%d %c%d\n", var0, idx0, var1, idx1);
+        break;
+      }
+      case 'v': {  /* atribuicao */
+        int idx0;
+        char var0 = c, c0;
+        if (fscanf(f, "%d = %c",&idx0, &c0) != 2)
+          error("comando invalido",line);
+
+        if (c0 == 'c') { /* call */
+          int func, idx1;
+          char var1;
+          if (fscanf(f, "all %d %c%d\n", &func, &var1, &idx1) != 3)
+            error("comando invalido",line);
+          printf("%c%d = call %d %c%d\n",var0, idx0, func, var1, idx1);
+        }
+        else { /* operação aritmética */
+          int idx1, idx2;
+          char var1 = c0, var2, op;
+          if (fscanf(f, "%d %c %c%d", &idx1, &op, &var2, &idx2) != 4)
+            error("comando invalido", line);
+          printf("%c%d = %c%d %c %c%d\n",
+                var0, idx0, var1, idx1, op, var2, idx2);
+        }
+        break;
+      }
+      default: error("comando desconhecido", line);
+    }
+    line ++;
+    fscanf(f, " ");
+  }
+  return;
+}
+
--- main.c
+++ main.c
@@ -1,81 +1,38 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include "gera_codigo.h"
 
-static void error (const char *msg, int line) {
-  fprintf(stderr, "erro %s na linha %d\n", msg, line);
-  exit(EXIT_FAILURE);
-}
-
-int main (void) {
-  int line = 1;
-  int  c;
-  FILE *myfp;
-
-  if ((myfp = fopen ("programa", "r")) == NULL) {
-    perror ("nao conseguiu abrir arquivo!");
-    exit(1);
+int main(int argc, char *argv[]) {
+  FILE *fp;
+  void *code;
+  funcp funcSBF;
+  int res;
+  
+  if( argc != 2 )
+  {
+  	printf("Numero invalido de parametros\n");
+    return 0;
   }
-
-  while ((c = fgetc(myfp)) != EOF) {
-    switch (c) {
-      case 'f': { /* function */
-        char c0;
-        if (fscanf(myfp, "unction%c", &c0) != 1)
-          error("comando invalido", line);
-        printf("function\n");
-        break;
-      }
-      case 'e': { /* end */
-        char c0;
-        if (fscanf(myfp, "nd%c", &c0) != 1)
-          error("comando invalido", line);
-        printf("end\n");
-        break;
-      }
-      case 'r': {  /* retorno incondicional */
-        int idx0, idx1;
-        char var0, var1;
-        if (fscanf(myfp, "et %c%d", &var0, &idx0) != 2) 
-          error("comando invalido", line);
-        printf("ret %c%d\n", var0, idx0);
-        break;
-      }
-      case 'z': {  /* retorno condicional */
-        int idx0, idx1;
-        char var0, var1;
-        if (fscanf(myfp, "ret %c%d %c%d", &var0, &idx0, &var1, &idx1) != 4) 
-          error("comando invalido", line);
-        printf("zret %c%d %c%d\n", var0, idx0, var1, idx1);
-        break;
-      }
-      case 'v': {  /* atribuicao */
-        int idx0;
-        char var0 = c, c0;
-        if (fscanf(myfp, "%d = %c",&idx0, &c0) != 2)
-          error("comando invalido",line);
-
-        if (c0 == 'c') { /* call */
-          int f, idx1;
-          char var1;
-          if (fscanf(myfp, "all %d %c%d\n", &f, &var1, &idx1) != 3)
-            error("comando invalido",line);
-          printf("%c%d = call %d %c%d\n",var0, idx0, f, var1, idx1);
-        }
-        else { /* operação aritmética */
-          int idx1, idx2;
-          char var1 = c0, var2, op;
-          if (fscanf(myfp, "%d %c %c%d", &idx1, &op, &var2, &idx2) != 4)
-            error("comando invalido", line);
-          printf("%c%d = %c%d %c %c%d\n",
-                var0, idx0, var1, idx1, op, var2, idx2);
-        }
-        break;
-      }
-      default: error("comando desconhecido", line);
-    }
-    line ++;
-    fscanf(myfp, " ");
+  
+  /* Abre o arquivo para leitura */
+  fp = fopen(argv[1], "r");
+  
+  if( fp == NULL )
+  {
+  	printf("Erro ao abrir o arquivo\n");
+    return 0;
   }
-  return 0;
+  
+  /* Gera o codigo */
+  gera_codigo(fp, &code, &funcSBF);
+  if ((code == NULL) || (funcSBF == NULL)) {
+    printf("Erro na geracao\n");
+    return 0;
+  }
+  
+  /* Chama a função gerada */
+  //res = (*funcSBF)(argv[2]);
+  
+  /* Libera a memória utilizada */
+  //libera_codigo(code);
+  
+  return 0;  
 }
